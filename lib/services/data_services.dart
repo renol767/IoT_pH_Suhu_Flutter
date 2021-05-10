5d81e5488eb1a396749de92779bd4e10283afc5f
renol767 <72129061+renol767@users.noreply.github.com>
2021-05-11 04:31:22 +0700

    Add Screenshot

--- lib/services/data_services.dart
+++ lib/services/data_services.dart
@@ -4,7 +4,7 @@ import '../models/datamodel.dart';
 
 class DataService {
   Future<Sensor> getData() async {
-    return http.get("http://192.168.1.20/dht11/api/sensor").then((data) {
+    return http.get("http://192.168.1.10/dht11/api/sensor").then((data) {
       if (data.statusCode == 200) {
         final jsonData = json.decode(data.body);
         return Sensor.fromJson(jsonData[0]);
